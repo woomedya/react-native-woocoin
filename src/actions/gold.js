b948adc39c7bdb97bbbfce00092106f8e1000d23
Woo Mobil <woomobil@Woo-iMac.local>
2020-10-28 09:30:22 +0300

    gold üretim hatası düzeltildi

--- src/actions/gold.js
+++ src/actions/gold.js
@@ -1,18 +1,14 @@
 import * as goldApi from '../apis/gold';
 
-var actions = {}, timeout = null;
-export const generateGold = (actionKey) => {
-    if (timeout) {
-        clearTimeout(timeout);
-        timeout = null;
+var actions = {};
+export const generateGold = async (actionKey) => {
+    if (actions[actionKey])
+        return;
+
+    actions[actionKey] = true;
+
+    if (actions[actionKey]) {
+        await goldApi.generateGold(actionKey);
+        actions[actionKey] = false;
     }
-    timeout = setTimeout(async () => {
-        if (actions[actionKey])
-            return;
-        actions[actionKey] = true;
-        if (actions[actionKey]) {
-            await goldApi.generateGold(actionKey);
-            actions[actionKey] = false;
-        }
-    }, 5000);
 }
