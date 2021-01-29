18852191f8a49a32cbc2528a9d509e483e5d618d
woomedya <woomedya06@gmail.com>
2021-01-29 11:31:07 +0300

    android woc üretim sesi yükseltildi

--- src/utilities/sound.js
+++ src/utilities/sound.js
@@ -1,4 +1,5 @@
 import Sound from 'react-native-sound';
+import { Platform } from 'react-native';
 
 export const COINS = 'coins';
 
@@ -12,6 +13,6 @@ export const play = (type) => {
     if (mp3[type]) {
         mp3[type].stop();
 
-        mp3[type].setVolume(0.3).play();
+        mp3[type].setVolume(Platform.OS == 'ios' ? 0.3 : 0.7).play();
     }
 }
